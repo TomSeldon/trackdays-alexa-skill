742ea903b314da2ab0f67976c16802575840de11
Tom Seldon <tom@tomseldon.co.uk>
2017-08-20 21:52:53 +0100

    [FEATURE] Modify help speech

--- messages/help.js
+++ messages/help.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const helpMessage =
-    `<p>I can help you find the next car or motorbike track day at venues across the UK and Europe.</p>` +
+    `<p>I can help you find information about upcoming car and motorbike track days at venues across the UK and Europe.</p>` +
     `<p>For example, you can ask me to find the next available car track day at Silverstone, ` +
     `or to find the next motorbike track day at Brands Hatch.</p>` +
     `<p>What would you like me to do?</p>`;
