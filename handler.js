75838b9e0f9a28b603ba2fde98947ca78f0b23b7
Tom Seldon <tom@tomseldon.co.uk>
2017-08-20 21:43:16 +0100

    [CONFIG] Add Alexa skill ID

--- handler.js
+++ handler.js
@@ -15,7 +15,7 @@ const handlers = {
 module.exports.handler = (event, context, callback) => {
     const alexa = Alexa.handler(event, context, callback);
 
-    alexa.APP_ID = '';
+    alexa.APP_ID = 'amzn1.ask.skill.f3de0848-097a-4cc5-b685-b2c92b3f2b39';
 
     alexa.registerHandlers(handlers);
     alexa.execute();
